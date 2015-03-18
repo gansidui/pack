9d09f53561f2a6cb40dcd6f0b18b54b335eb1953
Jie Li <626678853@qq.com>
2015-03-18 12:34:36 +0800

    add pack

---
+++ pack.go
@@ -0,0 +1,98 @@
+package pack
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type Pack struct {
+	buf *bytes.Buffer
+}
+
+func New() *Pack {
+	return &Pack{
+		buf: new(bytes.Buffer),
+	}
+}
+
+func NewWithNoCopy(buf []byte) *Pack {
+	return &Pack{
+		buf: bytes.NewBuffer(buf),
+	}
+}
+
+func (p *Pack) Reset() {
+	p.buf.Reset()
+}
+
+func (p *Pack) Len() int {
+	return p.buf.Len()
+}
+
+func (p *Pack) Bytes() []byte {
+	return p.buf.Bytes()
+}
+
+func (p *Pack) Next(n int) []byte {
+	return p.buf.Next(n)
+}
+
+func (p *Pack) Read(buf []byte) (n int, err error) {
+	return p.buf.Read(buf)
+}
+
+func (p *Pack) ReadUint8() (uint8, bool) {
+	buf := p.buf.Next(1)
+	if len(buf) != 1 {
+		return 0, false
+	}
+	return buf[0], true
+}
+
+func (p *Pack) ReadUint16() (uint16, bool) {
+	buf := p.buf.Next(2)
+	if len(buf) != 2 {
+		return 0, false
+	}
+	return binary.BigEndian.Uint16(buf), true
+}
+
+func (p *Pack) ReadUint32() (uint32, bool) {
+	buf := p.buf.Next(4)
+	if len(buf) != 4 {
+		return 0, false
+	}
+	return binary.BigEndian.Uint32(buf), true
+}
+
+func (p *Pack) ReadUint64() (uint64, bool) {
+	buf := p.buf.Next(8)
+	if len(buf) != 8 {
+		return 0, false
+	}
+	return binary.BigEndian.Uint64(buf), true
+}
+
+func (p *Pack) Write(b []byte) (n int, err error) {
+	return p.buf.Write(b)
+}
+
+func (p *Pack) WriteString(s string) (n int, err error) {
+	return p.buf.WriteString(s)
+}
+
+func (p *Pack) WriteUint8(v uint8) {
+	binary.Write(p.buf, binary.BigEndian, v)
+}
+
+func (p *Pack) WriteUint16(v uint16) {
+	binary.Write(p.buf, binary.BigEndian, v)
+}
+
+func (p *Pack) WriteUint32(v uint32) {
+	binary.Write(p.buf, binary.BigEndian, v)
+}
+
+func (p *Pack) WriteUint64(v uint64) {
+	binary.Write(p.buf, binary.BigEndian, v)
+}
---
+++ pack_test.go
@@ -0,0 +1,73 @@
+package pack
+
+import (
+	"testing"
+)
+
+func TestPack(t *testing.T) {
+	p := New()
+	if p.Len() != 0 {
+		t.Fatal()
+	}
+
+	p.WriteUint32(3200)
+	p.WriteUint16(1600)
+	p.WriteUint64(64)
+	p.WriteUint8(8)
+	p.Write([]byte("hello"))
+	p.WriteString("world")
+
+	if p.Len() != 25 {
+		t.Fatal()
+	}
+
+	if v, ok := p.ReadUint32(); !ok || v != 3200 {
+		t.Fatal()
+	}
+	if v, ok := p.ReadUint16(); !ok || v != 1600 {
+		t.Fatal()
+	}
+	if v, ok := p.ReadUint64(); !ok || v != 64 {
+		t.Fatal()
+	}
+	if v, ok := p.ReadUint8(); !ok || v != 8 {
+		t.Fatal()
+	}
+
+	buf := make([]byte, 10)
+	p.Read(buf[0:5])
+	if string(buf[0:5]) != "hello" {
+		t.Fatal()
+	}
+
+	p.Read(buf[5:10])
+	if string(buf[5:10]) != "world" {
+		t.Fatal()
+	}
+
+	if p.Len() != 0 {
+		t.Fatal()
+	}
+
+	p.WriteUint16(1314)
+	p.Reset()
+
+	if p.Len() != 0 {
+		t.Fatal()
+	}
+
+	p.Write([]byte("hello world"))
+	if string(p.Bytes()) != "hello world" {
+		t.Fatal()
+	}
+
+	buf = p.Next(5)
+	if string(buf) != "hello" || p.Len() != 6 {
+		t.Fatal()
+	}
+
+	buf = p.Next(6)
+	if string(buf) != " world" || p.Len() != 0 {
+		t.Fatal()
+	}
+}
